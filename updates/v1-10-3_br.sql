502e08b3b81e9e697a00a23f8f8f24c0e5a8aa11
David Hellebrand <david@hellebrand.eu>
2015-03-15 17:45:26 +0100

    uprava TAT a uctovani u vysetreni SMA

--- updates/v1-10-3_br.sql
+++ updates/v1-10-3_br.sql
@@ -4,5 +4,11 @@ INSERT INTO PUBLIC.USER(ID, ISADMIN, JMENO, PARAFA, PASSWORDHASH, POZICE, USERNA
 update user set id=13 where id=39;
 
 INSERT INTO PUBLIC.EXAMINATION(ID, AKTUAL, BODY, CERTIF, NAZEV, POPIS, SLOUPCE, VYSVETLIVKA, ZKRATKA, GENOMAC, TAT) VALUES
-(61, TRUE, 0, FALSE, STRINGDECODE('Spin\u00e1ln\u00ed muskul\u00e1rn\u00ed atrofie'), STRINGDECODE('Spin\u00e1ln\u00ed muskul\u00e1rn\u00ed atrofie: delece/duplikace exonu 7 a 8 v genech SMN1 a SMN2'), 1, STRINGDECODE('((+)) - Sledovan\u00fd \u00fasek nen\u00ed deletov\u00e1n, ani duplikov\u00e1n, ((del)) - Sledovan\u00fd \u00fasek je deletov\u00e1n, ((dup)) \u2013 Sledovan\u00fd \u00fasek je duplikov\u00e1n.'), 'SMA', FALSE, 0),
+(61, TRUE, 0, FALSE, STRINGDECODE('Spin\u00e1ln\u00ed muskul\u00e1rn\u00ed atrofie'), STRINGDECODE('Spin\u00e1ln\u00ed muskul\u00e1rn\u00ed atrofie: delece/duplikace exonu 7 a 8 v genech SMN1 a SMN2'), 1, STRINGDECODE('((+)) - Sledovan\u00fd \u00fasek nen\u00ed deletov\u00e1n, ani duplikov\u00e1n, ((del)) - Sledovan\u00fd \u00fasek je deletov\u00e1n, ((dup)) \u2013 Sledovan\u00fd \u00fasek je duplikov\u00e1n.'), 'SMA', FALSE, 10),
 (62, FALSE, 0, FALSE, STRINGDECODE('Stanoven\u00ed f\u00fazn\u00edho genu BCR-ABL'), STRINGDECODE('Stanoven\u00ed f\u00fazn\u00edho genu BCR-ABL (Ph1 chromozom): p\u0159estavby M bcr, m bcr a \u00b5 bcr, metodou RT PCR, SOP OV \u010d. 14'), 0, '', '', FALSE, 0);
+
+-- INSERT INTO PUBLIC.GENOTYPE(ID, NAZEV, VYCHOZI, VYSETRENI_ID, AUTOCOMPL, PORADI) VALUES
+-- (198, 'SMN1 exon 7', '', 61, '', 1),
+-- (199, 'SMN1 exon 8', '', 61, '', 2),
+-- (200, 'SMN2 exon 7', '', 61, '', 3),
+-- (201, 'SMN2 exon 8', '', 61, '', 4);
