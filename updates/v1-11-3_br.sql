18089ebf63479b7a4e6d5b8da6fe8dd6ad8fe222
dhe <david.hellebrand@seznam.cz>
2015-09-29 20:38:36 +0200

    verze 1.11.3

---
+++ updates/v1-11-3_br.sql
@@ -0,0 +1,4 @@
+delete from genotype where id >= 72 and id <= 73;
+delete from genotype where id >= 75 and id <= 81;
+update genotype set poradi = 2 where id=74;
+update score set pocet = 6 where id=2;
